91679292e300d37e72320931acbb967e0fc4ed37
Mei <137881837+minhotaku@users.noreply.github.com>
2024-04-27 20:09:36 +0700

    add

---
+++ Spirograph/main.py
@@ -0,0 +1,23 @@
+import random
+import turtle as t
+
+my_turtle = t.Turtle()
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return "#{:02x}{:02x}{:02x}".format(r, g, b)
+
+
+my_turtle.speed("fastest")
+for _ in range(36):
+    my_turtle.color(random_color())
+    my_turtle.circle(100)
+    current_heading = my_turtle.heading()
+    my_turtle.setheading(current_heading + 10)
+
+
+screen = t.Screen()
+screen.exitonclick()
